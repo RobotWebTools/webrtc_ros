31bbc9f7d134d2b5a4955f6b0430d7c1961c2fa0
Mitchell Wills <mwills@wpi.edu>
2014-11-26 23:51:04 -0500

    Fixed checkout source script to correctly checkout build files

--- webrtc/checkout_source.sh
+++ webrtc/checkout_source.sh
@@ -4,5 +4,4 @@ svn co http://webrtc.googlecode.com/svn/trunk webrtc_src
 svn co https://libyuv.googlecode.com/svn/trunk yuv_src
 if [ -d opus_src ]; then (cd opus_src && git pull); else git clone https://chromium.googlesource.com/chromium/deps/opus.git opus_src;fi
 svn co http://sctp-refimpl.googlecode.com/svn/trunk/KERN/usrsctp usrsctp_src
-mkdir -p chromium_src/build
-if [ -d chromium_src/build ]; then (cd chromium_src/build && git pull); else git clone https://chromium.googlesource.com/chromium/src/build chromium_src/build;fi
+if [ -d chromium_src/build ]; then (cd chromium_src/build && git pull); else (mkdir -p chromium_src/build && git clone https://chromium.googlesource.com/chromium/src/build chromium_src/build);fi

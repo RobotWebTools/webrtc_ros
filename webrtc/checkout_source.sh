72ee5a4a4aec0c112bead4301ee4a0ba6279a0e8
Mitchell Wills <mwills@wpi.edu>
2015-01-21 17:23:55 -0500

    BROKEN: now build libvpx from source

--- webrtc/checkout_source.sh
+++ webrtc/checkout_source.sh
@@ -5,11 +5,13 @@ svn co https://libyuv.googlecode.com/svn/branches/m39 yuv_src
 if [ -d opus_src ]; then (cd opus_src && git fetch); else git clone https://chromium.googlesource.com/chromium/deps/opus.git opus_src;fi; (cd opus_src && git checkout cae6961)
 svn co -r 9066 http://sctp-refimpl.googlecode.com/svn/trunk/KERN/usrsctp usrsctp_src
 if [ -d chromium_src/build ]; then (cd chromium_src/build && git fetch); else (mkdir -p chromium_src/build && git clone https://chromium.googlesource.com/chromium/src/build chromium_src/build);fi; (cd chromium_src/build && git checkout 877467e)
+if [ -d libvpx ]; then (cd libvpx && git pull); else git clone https://chromium.googlesource.com/webm/libvpx libvpx;fi
 
 echo
 echo "Source versions:"
 echo -n "chromium: "; (cd chromium_src/build && git rev-parse HEAD)
 echo -n "usrsctp: "; (cd usrsctp_src && svnversion .)
 echo -n "opus: "; (cd opus_src && git rev-parse HEAD)
+echo -n "libvpx: "; (cd libvpx && git rev-parse HEAD)
 echo -n "yuv: "; (cd yuv_src && svnversion .)
 echo -n "webrtc: "; (cd webrtc_src && svnversion .)

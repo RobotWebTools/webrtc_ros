85c34c5d36ee4cccea61b248053d0882a01c1707
Mitchell Wills <mwills@wpi.edu>
2015-02-05 14:52:25 -0500

    Check out a specific version of opus because it updated and broke things

--- webrtc/checkout_source.sh
+++ webrtc/checkout_source.sh
@@ -2,7 +2,7 @@
 
 svn co http://webrtc.googlecode.com/svn/branches/40 webrtc_src
 svn co https://libyuv.googlecode.com/svn/branches/m39 yuv_src
-if [ -d opus_src ]; then (cd opus_src && git pull); else git clone https://chromium.googlesource.com/chromium/deps/opus.git opus_src;fi
+if [ -d opus_src ]; then (cd opus_src && git fetch); else git clone https://chromium.googlesource.com/chromium/deps/opus.git opus_src;fi; (cd opus_src && git checkout cae6961)
 svn co -r 9066 http://sctp-refimpl.googlecode.com/svn/trunk/KERN/usrsctp usrsctp_src
 if [ -d chromium_src/build ]; then (cd chromium_src/build && git fetch); else (mkdir -p chromium_src/build && git clone https://chromium.googlesource.com/chromium/src/build chromium_src/build);fi; (cd chromium_src/build && git checkout 877467e)
 

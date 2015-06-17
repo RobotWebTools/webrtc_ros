d0e15a64c1f831a37eadd2c3f7e06be87d1aabb6
Mitchell Wills <mwills@wpi.edu>
2015-06-17 15:45:53 -0400

    Cleaned up main web page
    Better custom subscribe form
    Added all image topics, not just ones with camera_info

---
+++ webrtc_ros/web/index.js
@@ -0,0 +1,54 @@
+function onload() {
+    var data_request = new XMLHttpRequest();
+    data_request.onload = function() {
+	if(data_request.status == 200) {
+	    var stream_data = JSON.parse(data_request.responseText);
+	    var camera_topics_list = document.getElementById("camera_topics_list");
+	    for(var base_topic in stream_data.camera_topics) {
+		var camera_li = document.createElement("li");
+		camera_li.appendChild(document.createTextNode(base_topic));
+
+		var topic_list = document.createElement("ul");
+		for(var topic_name in stream_data.camera_topics[base_topic]) {
+		    var topic_li = document.createElement("li");
+		    var topic_a = document.createElement("a");
+		    topic_a.appendChild(document.createTextNode(topic_name));
+		    topic_a.href = "/viewer?subscribe_video=ros_image:" + stream_data.camera_topics[base_topic][topic_name];
+		    topic_li.appendChild(topic_a);
+		    topic_list.appendChild(topic_li);
+		}
+		camera_li.appendChild(topic_list);
+
+		camera_topics_list.appendChild(camera_li);
+	    }
+
+	    var image_topic_list = document.getElementById("image_topics_list");
+	    for(var i in stream_data.image_topics) {
+		var topic = stream_data.image_topics[i];
+		var topic_li = document.createElement("li");
+		var topic_a = document.createElement("a");
+		topic_a.appendChild(document.createTextNode(topic));
+		topic_a.href = "/viewer?subscribe_video=ros_image:" + stream_data.camera_topics[base_topic][topic_name];
+		topic_li.appendChild(topic_a);
+		image_topic_list.appendChild(topic_li);
+	    }
+	}
+    };
+    data_request.open("GET", "/list_streams.json", true);
+    data_request.send();
+
+    document.getElementById("custom_topic_form").addEventListener("submit", function(event) {
+	var form = event.target;
+	var params = [];
+	if(form.publish_video_topic.value.length > 0)
+	    params.push("publish_video=ros_image:" + form.publish_video_topic.value);
+	if(form.subscribe_video_topic.value.length > 0)
+	    params.push("subscribe_video=ros_image:" + form.subscribe_video_topic.value);
+	if(form.publish_local_audio.checked)
+	    params.push("publish_audio=local:");
+	if(form.subscribe_local_audio.checked)
+	    params.push("subscribe_audio=local:");
+	event.preventDefault();
+	window.location = "/viewer?" + params.join("&");
+    });
+};

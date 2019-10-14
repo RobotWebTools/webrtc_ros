10e07b8a2327eb43485a92d3e96161b31d19059d
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2019-10-14 10:55:49 +0200

    Update JavaScript client code for latest browser compatibility

--- webrtc_ros/web/index.js
+++ webrtc_ros/web/index.js
@@ -1,84 +1,40 @@
-function onload() {
-    var data_request = new XMLHttpRequest();
-    data_request.onload = function()
-    {
-        if (data_request.status === 200)
-        {
-            var found = false;
-            var stream_data = JSON.parse(data_request.responseText);
-            var camera_topics_list = document.getElementById("camera_topics_list");
-            for (var base_topic in stream_data.camera_topics)
-            {
-                if (stream_data.camera_topics.hasOwnProperty(base_topic))
-                {
-                    found = true;
-                    var camera_li = document.createElement("li");
-                    camera_li.appendChild(document.createTextNode(base_topic));
-                    var camera_topic_list = document.createElement("ul");
-                    for (var topic_name in stream_data.camera_topics[base_topic])
-                    {
-                        if(stream_data.camera_topics[base_topic].hasOwnProperty(topic_name))
-                        {
-                            var camera_topic_li = document.createElement("li");
-                            var camera_topic_a = document.createElement("a");
-                            camera_topic_a.appendChild(document.createTextNode(topic_name));
-                            camera_topic_a.href = "/viewer?subscribe_video=ros_image:" + stream_data.camera_topics[base_topic][topic_name];
-                            camera_topic_li.appendChild(camera_topic_a);
-                            camera_topic_list.appendChild(camera_topic_li);
-                        }
-                    }
-                    camera_li.appendChild(camera_topic_list);
-                    camera_topics_list.appendChild(camera_li);
-                }
-            }
-            var image_topic_list = document.getElementById("image_topics_list");
-            for (var i in stream_data.image_topics)
-            {
-                if (stream_data.image_topics.hasOwnProperty(i))
-                {
-                    found = true;
-                    var topic = stream_data.image_topics[i];
-                    var topic_li = document.createElement("li");
-                    var topic_a = document.createElement("a");
-                    topic_a.appendChild(document.createTextNode(topic));
-                    topic_a.href = "/viewer?subscribe_video=ros_image:" + topic;
-                    topic_li.appendChild(topic_a);
-                    image_topic_list.appendChild(topic_li);
-                }
-            }
-            if (!found)
-            {
-                document.getElementById("status").appendChild(document.createTextNode("No topics found"));
-            }
-        }
-        else
-        {
-            document.getElementById("status").appendChild(document.createTextNode("Query failed (HTTP status "+ data_request.status + ")"));
-        }
-    };
-    data_request.open("GET", "/list_streams.json", true);
-    data_request.send();
-    document.getElementById("custom_topic_form").addEventListener("submit", function(event)
-    {
-        var form = event.target;
-        var params = [];
-        if (form.publish_video_topic.value.length > 0)
-        {
-            params.push("publish_video=ros_image:" + form.publish_video_topic.value);
-        }
-        if (form.subscribe_video_topic.value.length > 0)
-        {
-            params.push("subscribe_video=ros_image:" + form.subscribe_video_topic.value);
-        }
-        if (form.publish_local_audio.checked)
-        {
-            params.push("publish_audio=local:");
-        }
-        if(form.subscribe_local_audio.checked)
-        {
-            params.push("subscribe_audio=local:");
-        }
-        event.preventDefault();
-        window.location = "/viewer?" + params.join("&");
-    });
-}
+$(document).ready(function() {
+	$("#status").empty().text("Looking for available streams...");
+	$.getJSON("list_streams.json", function(json) {
+		for (var base_topic in json.camera_topics)
+		{
+			if (json.camera_topics.hasOwnProperty(base_topic))
+			{
+				var camera_li = $("<li>");
+				camera_li.text(base_topic);
+				var topic_ul = $("<ul>");
+				for (var topic_name in json.camera_topics[base_topic])
+				{
+					if (json.camera_topics[base_topic].hasOwnProperty(topic_name))
+					{
+						topic_ul.append(
+							$("<li>").append(
+								$("<a>").attr("href", "viewer?subscribe_video=ros_image:" + json.camera_topics[base_topic][topic_name]).text(topic_name)
+							)
+						);
+					}
+				}
+				camera_li.append(topic_ul);
+				$("#camera_topics_list").append(camera_li);
+			}
+		}
+		for (var i in json.image_topics)
+		{
+			if (json.image_topics.hasOwnProperty(i))
+			{
+				$("#image_topics_list").append(
+					$("<li>").append(
+						$("<a>").attr("href", "viewer?subscribe_video=ros_image:" + json.image_topics[i]).text(json.image_topics[i])
+					)
+				);
+			}
+		}
+		$("#status").empty();
+	});
+});
+
--- webrtc_ros/web/viewer.js
+++ webrtc_ros/web/viewer.js
@@ -1,117 +1,43 @@
-var params = {};
-if(window.location.search.length > 1) {
-    var query_string = decodeURIComponent(window.location.search.substring(1));
-    var parameter_key_values = query_string.split("&");
-    for(var i in parameter_key_values) {
-	if(parameter_key_values.hasOwnProperty(i)) {
-	    var key_value = parameter_key_values[i];
-	    if(key_value.length > 0) {
-		var key_value_arr = key_value.split("=", 2);
-		if(key_value_arr.length === 2) {
-		    params[key_value_arr[0]] = key_value_arr[1];
+$(document).ready(function() {
+	var Q = {};
+	var pv = window.location.search.substring(1).split("&");
+	for (var i in pv)
+	{
+		if (pv.hasOwnProperty(i))
+		{
+			var kv = pv[i].split("=", 2);
+			if (kv.length == 2)
+			{
+				Q[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1]);
+			}
 		}
-		else {
-		    params[key_value_arr[0]] = "";
+	}
+	if (Q.subscribe_video)
+	{
+		$("#topic").text(Q.subscribe_video);
+		console.log("Establishing WebRTC connection");
+		var conn = WebrtcRos.createConnection();
+		conn.onConfigurationNeeded = function()
+		{
+			console.log("Requesting WebRTC video subscription");
+			var config = {};
+			config.video = {"id": "subscribed_video", "src": Q.subscribe_video};
+			conn.addRemoteStream(config).then(function(event) {
+				console.log("Connecting WebRTC stream to <video> element");
+				$("#remote-video").prop("srcObject", event.stream);
+				event.remove.then(function(event) {
+					console.log("Disconnecting WebRTC stream from <video> element");
+					$("#remote-video").prop("srcObject", null);
+				});
+			});
+			conn.sendConfigure();
 		}
-	    }
+		conn.connect();
 	}
-    }
-}
-
-var subscribe_video = (params.subscribe_video && params.subscribe_video.length > 0) ? params.subscribe_video : null;
-var publish_video = (params.publish_video && params.publish_video.length > 0) ? params.publish_video : null;
-
-var subscribe_audio = (params.subscribe_audio && params.subscribe_audio.length > 0) ? params.subscribe_audio : null;
-var publish_audio = (params.publish_audio && params.publish_audio.length > 0) ? params.publish_audio : null;
-
-var connection = WebrtcRos.createConnection();
-connection.onConfigurationNeeded = function() {
-    var config_box = document.getElementById("config-box");
-
-    config_box.innerHTML = "";
-    if(subscribe_video) {
-	config_box.innerHTML += "Subscribed Video: <b>" + subscribe_video + "</b>";
-    }
-    else {
-	config_box.innerHTML += "Not Subscribed to Video";
-    }
-    config_box.innerHTML += "<br>";
-
-    if(publish_video) {
-	config_box.innerHTML += "Published Video: <b>" + publish_video + "</b>";
-    }
-    else {
-	config_box.innerHTML += "Not Publishing Video";
-    }
-    config_box.innerHTML += "<br>";
-
-    if(subscribe_audio) {
-	config_box.innerHTML += "Subscribed Audio: <b>" + subscribe_audio + "</b>";
-    }
-    else {
-	config_box.innerHTML += "Not Subscribed to Audio";
-    }
-    config_box.innerHTML += "<br>";
-
-    if(publish_audio) {
-	config_box.innerHTML += "Published Audio: <b>" + publish_audio + "</b>";
-    }
-    else {
-	config_box.innerHTML += "Not Publishing Audio";
-    }
-    config_box.innerHTML += "<br>";
-
-
-    if(subscribe_video || subscribe_audio) {
-	var remote_stream_config = {};
-	if(subscribe_video) {
-	    remote_stream_config.video = {
-		"id": "subscribed_video",
-		"src": subscribe_video
-	    };
+	else
+	{
+		$("#topic").append("not connected");
+		console.log("No subscribe_video parameter, not connecting to WebRTC video stream");
 	}
-	if(subscribe_audio) {
-	    remote_stream_config.audio = {
-		"id": "subscribed_audio",
-		"src": subscribe_audio
-	    };
-	}
-	connection.addRemoteStream(remote_stream_config).then(function(event) {
-	    console.log("Remote stream added", event, event.stream.getVideoTracks(), event.stream.getAudioTracks());
-	    var remoteVideoElement = document.getElementById("remote-video");
-	    remoteVideoElement.srcObject = event.stream;
-	    event.remove.then(function(event){
-		console.log("Remote stream removed", event);
-		remoteVideoElement.srcObject = null;
-	    });
-	    window.remotestream = event.stream;
-	});
-    }
+});
 
-    if(publish_video || publish_audio) {
-	var user_media_config = {};
-	var local_stream_config = {};
-	if(publish_video) {
-	    user_media_config.video = true;
-	    local_stream_config.video = {
-		"dest": publish_video
-	    };
-	}
-	if(publish_audio) {
-	    user_media_config.audio = true;
-	}
-
-	connection.addLocalStream(user_media_config, local_stream_config).then(function(event) {
-	    console.log("Local stream added", event, event.stream.getVideoTracks(), event.stream.getAudioTracks());
-	    var localVideoElement = document.getElementById("local-video");
-	    localVideoElement.srcObject = event.stream;
-	    event.remove.then(function(event){
-		console.log("Local stream removed", event);
-		localVideoElement.srcObject = null;
-	    });
-	    window.localstream = event.stream;
-	});
-    }
-    connection.sendConfigure();
-};
-connection.connect();

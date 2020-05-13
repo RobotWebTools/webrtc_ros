a2a19dab35927ac79aceed62d766a62134b832c4
Michael Sobrepera <mjsobrep@live.com>
2020-05-13 19:06:53 -0400

    Fix errors in turn credential generation (#46)
    * Changed parameter to be private
    
    The parameters for the ICE Server Provider are not likely to be needed
    by anything else. There is no need to be cluttering up the global
    parameter space.
    
    * Added uri into turn definition.
    
    The URI was not being set on turn server definitions
    
    * Significant fixes to REST call for getting turn credentials
    
    The REST call was not looking for data in a valid field and was
    missing appropriate checks for failed requests. Also added in
    logging to allow future diagnostics

--- webrtc_ros/scripts/ice_server_service.py
+++ webrtc_ros/scripts/ice_server_service.py
@@ -13,14 +13,14 @@ class IceServerManager(object):
     def __init__(self):
         rospy.init_node('ice_server_provider')
 
-        self.stun_servers = rospy.get_param('stun_servers', [
+        self.stun_servers = rospy.get_param('~stun_servers', [
             'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'])
-        self.turn_server_uris = rospy.get_param('turn_server_uris', '')
-        self.turn_creds_uri = rospy.get_param('turn_server_creds_uri', '')
+        self.turn_server_uris = rospy.get_param('~turn_server_uris', '')
+        self.turn_creds_uri = rospy.get_param('~turn_server_creds_uri', '')
         self.turn_creds_username = rospy.get_param(
-            'turn_server_creds_username', '')
+            '~turn_server_creds_username', '')
         self.turn_creds_password = rospy.get_param(
-            'turn_server_creds_password', '')
+            '~turn_server_creds_password', '')
 
         self.get_ice_servers_service = rospy.Service(
             'get_ice_servers', GetIceServers, self.get_ice_servers)
@@ -31,11 +31,24 @@ class IceServerManager(object):
     def get_turn_creds(self):
         """Get the credentials from the turn server."""
         if self.turn_creds_uri:
+            rospy.logdebug('getting turn server credentials')
             resp = requests.post(self.turn_creds_uri,
                                  {'username': self.turn_creds_username,
                                   'password':  self.turn_creds_password})
-            if(('username' in resp.data) and ('password' in resp.data)):
-                return resp.data
+            try:
+                rospy.loginfo('trying to parse response from server')
+                data = resp.json()
+                if('username' in data and 'password' in data):
+                    rospy.loginfo('succesfully received turn credentials')
+                    return data
+                rospy.logwarn(
+                    'response did not have username and password fields')
+            except AttributeError:
+                rospy.logerr(
+                    'server did not respond with JSON, response code: %i',
+                    resp.status_code)
+        else:
+            rospy.logdebug('No uri provided for turn credentials')
         return False
 
     def get_ice_servers(self, _):
@@ -45,6 +58,7 @@ class IceServerManager(object):
         if turn_creds:
             for uri in self.turn_server_uris:
                 serv = IceServer()
+                serv.uri = uri
                 serv.username = turn_creds['username']
                 serv.password = turn_creds['password']
                 resp.servers.append(serv)
